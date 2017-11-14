095ee2ee0a5991a6c61c894d34de0a1ed1abdba8
Joanthan Hughes <jonathan.m.hughes.1@gmail.com>
2017-11-13 20:47:42 -0500

    CalculatePay method calculates pay correctly

---
+++ BabysitterCalculator/BabysitterCalculator/Source/BabysitterCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BabysitterCalculator.Source
+{
+    public class BabysitterCalculator
+    {
+        public BabysitterCalculator()
+        {
+        }
+
+        public decimal CalculatePay(int hoursWorked, decimal payRate)
+        {
+            return hoursWorked * payRate;
+        }
+    }
+}
---
+++ BabysitterCalculator/BabysitterCalculator/Tests/BabysitterCalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿namespace BabysitterCalculator.Tests
+{
+    using Source;
+    using Xunit;
+    using System;
+    using FluentAssertions;
+
+    public class BabysitterCalculatorTests
+    {
+        BabysitterCalculator babysitterCalculator;
+
+        public BabysitterCalculatorTests()
+        {
+            babysitterCalculator = new BabysitterCalculator();
+        }
+
+        [Theory]
+        [InlineData(3, 12, 36)]
+        [InlineData(4, 8, 32)]
+        [InlineData(5, 16, 80)]
+        public void CalculatePay(int hoursWorked, decimal payRate, decimal expectedResult)
+        {
+            (babysitterCalculator.CalculatePay(hoursWorked, payRate)).Should().Be(expectedResult);
+        }
+    }
+}
