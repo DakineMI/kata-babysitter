4ab950d2203b4632c3fe69d7c10ccd09dfe83d09
Joanthan Hughes <jonathan.m.hughes.1@gmail.com>
2017-11-14 11:00:04 -0500

    Adding WorkTypeHours to determine how many hours of each worktype to bill for

---
+++ BabysitterCalculator/BabysitterCalculator/WorkType/Source/WorkTypeHours.cs
@@ -0,0 +1,63 @@
+﻿namespace BabysitterCalculator.WorkType.Source
+{
+    using System;
+    using System.Collections.Generic;
+    public class WorkTypeHours
+    {
+        public Dictionary<WorkHourType, int> GetTheNumberOfHoursByWorkType(DateTime startTime, DateTime endTime, DateTime bedTime)
+        {
+            if ((endTime - startTime).Seconds < 0)
+                throw new ArgumentOutOfRangeException($"The end time must after the start time");
+
+            var start = new DateTime(startTime.Year, startTime.Month, startTime.Day, startTime.Hour, 0, 0);
+            var stop = new DateTime(endTime.Year, endTime.Month, endTime.Day, endTime.Hour, 0, 0);
+            if (endTime.Minute > 0 || endTime.Second > 0)
+                stop = stop.AddHours(1);
+            var bed = new DateTime(bedTime.Year, bedTime.Month, bedTime.Day, bedTime.Hour, 0, 0);
+            if (bedTime.Minute > 0 || bedTime.Second > 0)
+                bed = bed.AddHours(1);
+            var midnight = new DateTime(stop.Year, stop.Month, stop.Day, 0, 0, 0);
+            if (stop.Hour > 17)
+                midnight = midnight.AddDays(1);
+
+            if (start.Hour < 17 && start.Hour > 4)
+                throw new ArgumentOutOfRangeException($"The start time must be between 5 PM and 4 AM");
+            if (stop.Hour < 17 && stop.Hour > 4)
+                throw new ArgumentOutOfRangeException($"The end time must be between 5 PM and 4 AM");
+            
+            Dictionary<WorkHourType, int> workTypeHours = new Dictionary<WorkHourType, int>();
+
+            int bedTimeHours = 0;
+            int defaultHours = 0;
+            int afterMidnightHours = 0;
+
+            //If the start time is after midnight, there will only be after midnight hours
+            if(start >= midnight)
+            {
+                if (stop == start)
+                    afterMidnightHours = 1;
+                else if (stop > start)
+                    afterMidnightHours = (stop - start).Hours;
+            }            
+            else if (stop <= midnight) //Start and end times are before midnight
+            {
+                if (bed >= start && bed < stop)
+                    bedTimeHours = (stop - bed).Hours;
+                defaultHours = (stop - start).Hours - bedTimeHours;
+            }
+            else
+            {
+                if (bed >= start && bed < midnight)
+                    bedTimeHours = (midnight - bed).Hours;
+                defaultHours = (midnight - start).Hours - bedTimeHours;
+                afterMidnightHours = (stop - midnight).Hours;
+            }
+
+            workTypeHours.Add(WorkHourType.Default, defaultHours);
+            workTypeHours.Add(WorkHourType.BedTime, bedTimeHours);
+            workTypeHours.Add(WorkHourType.AfterMidnight, afterMidnightHours);
+
+            return workTypeHours;
+        }
+    }
+}
---
+++ BabysitterCalculator/BabysitterCalculator/WorkType/Tests/WorkTypeHoursTests.cs
@@ -0,0 +1,146 @@
+﻿namespace BabysitterCalculator.WorkType.Tests
+{
+    using FluentAssertions;
+    using Source;
+    using System;
+    using Xunit;
+
+    public class WorkTypeHoursTests
+    {
+
+        [Fact]
+        public void ReturnsCorrectWorkTypeHoursWithBedTimeBeforeStartTimeAndEndTimeAfterMidnight()
+        {
+            DateTime startTime = DateTime.Now.Date.AddHours(-5).AddMinutes(-10);
+            DateTime endTime = DateTime.Now.Date.AddMinutes(2);
+            DateTime bedTime = DateTime.Now.Date.AddHours(-7);
+
+            var x = new WorkTypeHours();
+
+            var result = x.GetTheNumberOfHoursByWorkType(startTime, endTime, bedTime);
+            result[WorkHourType.Default].Should().Be(6, "The default billable time is from 6 till midnight");
+            result[WorkHourType.BedTime].Should().Be(0, "The bed time billable time is before the start time");
+            result[WorkHourType.AfterMidnight].Should().Be(1, "The after midnight billable time is from midnight till 1");
+        }
+
+        [Fact]
+        public void ReturnsCorrectWorkTypeHoursWithBedTimeAfterStartTimeAndEndTimeAfterMidnight()
+        {
+            DateTime startTime = DateTime.Now.Date.AddHours(-5).AddMinutes(-10);
+            DateTime endTime = DateTime.Now.Date.AddMinutes(2);
+            DateTime bedTime = DateTime.Now.Date.AddHours(-2);
+
+            var x = new WorkTypeHours();
+
+            var result = x.GetTheNumberOfHoursByWorkType(startTime, endTime, bedTime);
+            result[WorkHourType.Default].Should().Be(4, "The default billable time is from 6 till 10");
+            result[WorkHourType.BedTime].Should().Be(2, "The bed time billable time is from 10 till midnight");
+            result[WorkHourType.AfterMidnight].Should().Be(1, "The after midnight billable time is from midnight till 1");
+        }
+
+        [Fact]
+        public void ReturnsCorrectWorkTypeHoursWithBedTimeAfterStartTimeAndEndTimeBeforeMidnight()
+        {
+            DateTime startTime = DateTime.Now.Date.AddHours(-5).AddMinutes(-10);
+            DateTime endTime = DateTime.Now.Date.AddMinutes(-2);
+            DateTime bedTime = DateTime.Now.Date.AddHours(-2);
+
+            var x = new WorkTypeHours();
+
+            var result = x.GetTheNumberOfHoursByWorkType(startTime, endTime, bedTime);
+            result[WorkHourType.Default].Should().Be(4, "The default billable time is from 6 till 10");
+            result[WorkHourType.BedTime].Should().Be(2, "The bed time billable time is from 10 till midnight");
+            result[WorkHourType.AfterMidnight].Should().Be(0, "The end time is before midnight");
+        }
+
+        [Fact]
+        public void ReturnsCorrectWorkTypeHoursWithBedTimeBeforeStartTimeAndEndTimeBeforeMidnight()
+        {
+            DateTime startTime = DateTime.Now.Date.AddHours(-5).AddMinutes(-10);
+            DateTime endTime = DateTime.Now.Date.AddMinutes(-2);
+            DateTime bedTime = DateTime.Now.Date.AddHours(-7);
+
+            var x = new WorkTypeHours();
+
+            var result = x.GetTheNumberOfHoursByWorkType(startTime, endTime, bedTime);
+            result[WorkHourType.Default].Should().Be(6, "The default billable time is from 6 till midnight");
+            result[WorkHourType.BedTime].Should().Be(0, "The bed time billable time is before the start time");
+            result[WorkHourType.AfterMidnight].Should().Be(0, "The end time is before midnight");
+        }
+
+        [Fact]
+        public void ReturnsCorrectWorkTypeHoursWithBedTimeAfterTheEndTimeAndEndTimeBeforeMidnight()
+        {
+            DateTime startTime = DateTime.Now.Date.AddHours(-5).AddMinutes(-10);
+            DateTime endTime = DateTime.Now.Date.AddMinutes(-5);
+            DateTime bedTime = DateTime.Now.Date.AddMinutes(-2);
+
+            var x = new WorkTypeHours();
+
+            var result = x.GetTheNumberOfHoursByWorkType(startTime, endTime, bedTime);
+            result[WorkHourType.Default].Should().Be(6, "The default billable time is from 6 till midnight");
+            result[WorkHourType.BedTime].Should().Be(0, "The bed time billable time is after the end time");
+            result[WorkHourType.AfterMidnight].Should().Be(0, "The end time is before midnight");
+        }
+
+        [Fact]
+        public void ReturnsCorrectWorkTypeHoursWithBedTimeAndEndTimeAfterMidnight()
+        {
+            DateTime startTime = DateTime.Now.Date.AddHours(-5).AddMinutes(-10);
+            DateTime endTime = DateTime.Now.Date.AddHours(4);
+            DateTime bedTime = DateTime.Now.Date.AddHours(2);
+
+            var x = new WorkTypeHours();
+
+            var result = x.GetTheNumberOfHoursByWorkType(startTime, endTime, bedTime);
+            result[WorkHourType.Default].Should().Be(6, "The default billable time is from 6 till midnight");
+            result[WorkHourType.BedTime].Should().Be(0, "The bed time billable time is after midnight");
+            result[WorkHourType.AfterMidnight].Should().Be(4, "The after midnight billable time is from midnight till 4");
+        }
+
+        [Fact]
+        public void ReturnsCorrectWorkTypeHoursWithStartTimeAndEndTimeAfterMidnight()
+        {
+            DateTime startTime = DateTime.Now.Date.AddHours(1);
+            DateTime endTime = DateTime.Now.Date.AddHours(4);
+            DateTime bedTime = DateTime.Now.Date.AddHours(2);
+
+            var x = new WorkTypeHours();
+
+            var result = x.GetTheNumberOfHoursByWorkType(startTime, endTime, bedTime);
+            result[WorkHourType.Default].Should().Be(0, "The default billable time is after midnight");
+            result[WorkHourType.BedTime].Should().Be(0, "The bed time billable time is after midnight");
+            result[WorkHourType.AfterMidnight].Should().Be(3, "The after midnight billable time is from 1 till 4");
+        }
+
+        [Fact]
+        public void ReturnsCorrectWorkTypeHoursWithBedTimeEqualToStartTimeAndEndTimeAfterMidnight()
+        {
+            DateTime startTime = DateTime.Now.Date.AddHours(-6);
+            DateTime endTime = DateTime.Now.Date.AddHours(2);
+            DateTime bedTime = DateTime.Now.Date.AddHours(-6);
+
+            var x = new WorkTypeHours();
+
+            var result = x.GetTheNumberOfHoursByWorkType(startTime, endTime, bedTime);
+            result[WorkHourType.Default].Should().Be(0, "The default billable time starts at bedtime");
+            result[WorkHourType.BedTime].Should().Be(6, "The bed time billable time is from 6 till midnight");
+            result[WorkHourType.AfterMidnight].Should().Be(2, "The after midnight billable time is from midnight till 1");
+        }
+
+        [Fact]
+        public void ReturnsCorrectWorkTypeHoursWithBedTimeEqualToStartTimeAndEndTimeBeforeMidnight()
+        {
+            DateTime startTime = DateTime.Now.Date.AddHours(-6);
+            DateTime endTime = DateTime.Now.Date.AddHours(-1);
+            DateTime bedTime = DateTime.Now.Date.AddHours(-6);
+
+            var x = new WorkTypeHours();
+
+            var result = x.GetTheNumberOfHoursByWorkType(startTime, endTime, bedTime);
+            result[WorkHourType.Default].Should().Be(0, "The default billable time starts at bedtime");
+            result[WorkHourType.BedTime].Should().Be(5, "The bed time billable time is from 6 till 11");
+            result[WorkHourType.AfterMidnight].Should().Be(0, "The end time is before midnight");
+        }
+    }
+}
